3e4c33860afc3dd362f57cb27a4f4449558ff1a7
Vaibhav-Student <vaibhavbeladiya1112@gmail.com>
2025-03-21 17:36:20 +0530

    initial commit.

---
+++ lib/first_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:navigation_push_pop/second_page.dart';
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondPage()),
+            );
+          },
+          child: Text('Go to Second Page'),
+        ),
+      ),
+    );
+  }
+}
